471fc912ba43b36b75ddc1e898ca68eef13dabbf
Castillou <joajoa70584@gmail.com>
2024-10-14 08:53:12 +0900

    README.md update

--- Js/background.js
+++ Js/background.js
@@ -6,10 +6,10 @@
 
     ※ Math.floor(Math.random() * Number)
     : 0~Number 사이의 랜덤한 숫자를 추출
-*/ 
+*/
 
-const images = ["0.jpg", "1.jpg", "2.jpg"];
-const wrap = document.querySelector(".wrap");
+const images = ['0.jpg', '1.jpg', '2.jpg'];
+const wrap = document.querySelector('.wrap');
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
---
+++ README.md
@@ -0,0 +1,3 @@
+# MOMENTUM Web Applicatoin
+
+모멘텀 웹 어플리케이션 클론 코딩 기록
